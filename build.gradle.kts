54f86e67f7597fda8ebad599cf3b20fd54e9bf06
Essem <smswessem@gmail.com>
2023-12-29 15:23:23 -0600

    Update everything

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+    `maven-publish`
+    alias(libs.plugins.lavalink)
+}
+
+group = "space.essem"
+version = "0.2.2"
+
+lavalinkPlugin {
+    name = "lava-xm-plugin"
+    apiVersion = libs.versions.lavalink.api
+    serverVersion = libs.versions.lavalink.server
+}
+
+repositories {
+    maven(url = "https://dankmemeitthefrog.github.io/maven-repo")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+dependencies {
+    implementation(libs.libxmp)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "lava-xm-plugin"
